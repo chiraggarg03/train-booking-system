e0ae42a1fdf537dafd41cfd6915bce0e5d4aa528
Chirag Garg <chiraggarg@Chirags-MacBook-Air.local>
2025-07-29 22:59:58 +0530

    add

---
+++ includes/db.php
@@ -0,0 +1,5 @@
+<?php
+$host = "localhost"; $user = "root"; $password = ""; $dbname = "railway";
+$conn = new mysqli($host, $user, $password, $dbname);
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+?>
